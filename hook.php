8479f331eab03830ff974575c2b5c4172d50b7ce
loginxx <3445056435@qq.com>
2018-11-23 13:06:27 +0800

    ss

--- hook.php
+++ hook.php
@@ -1,9 +1,9 @@
 <?
 
-<<<<<<< HEAD
+
 echo '1234567';
-=======
+
 #echo '1234567';
 
-echo exec ('woami');
->>>>>>> 1c599e9cebbd8c2e73410145d8cd3f6ffc7f6fbb
+echo exec ('woami')
+1c599e9cebbd8c2e73410145d8cd3f6ffc7f6fbb
